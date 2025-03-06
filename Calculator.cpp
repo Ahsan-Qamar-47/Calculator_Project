42854f12fae9f54fae50da72df4aea1815d99602
Ahsan-Qamar-47 <ahsanqamar926@gmail.com>
2025-03-06 14:12:06 +0500

    Division Function Added

--- Calculator.cpp
+++ Calculator.cpp
@@ -3,5 +3,10 @@ using namespace std;
 
 int main() {
     cout << "Your Name, Roll Number" << endl;
+    if (b != 0) {
+    cout << "Quotient: " << a / b << endl;
+} else {
+    cout << "Division by zero error!" << endl;
+}
     return 0;
 }
